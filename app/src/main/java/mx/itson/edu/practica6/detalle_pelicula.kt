85e9c52ec64d3f02b72fb206725533b85cfa345f
Jorge Andrés Figueroa Márquez <jorge.figueroa228692@potros.itson.edu.mx>
2024-03-07 15:50:50 -0700

    feat: Seat Selector and Buy Tickets

--- app/src/main/java/mx/itson/edu/practica6/detalle_pelicula.kt
+++ app/src/main/java/mx/itson/edu/practica6/detalle_pelicula.kt
@@ -1,9 +1,12 @@
 package mx.itson.edu.practica6
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 
 class detalle_pelicula : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +17,22 @@ class detalle_pelicula : AppCompatActivity() {
         val  tv_nombre_pelicula: TextView = findViewById(R.id.tv_nombre_pelicula)
         val tv_pelicula_desc: TextView = findViewById(R.id.tv_pelicula_desc)
 
+        val ticket: Button = findViewById(R.id.buyTickets)
+        val seats: TextView = findViewById(R.id.seatLeft)
+        val boletos: String? = intent.getStringExtra("seats")
+
+        Toast.makeText(this, "Boletos:" + boletos, Toast.LENGTH_LONG).show()
+        ticket.setOnClickListener {
+            var intent: Intent = Intent(this, SeatSelection::class.java)
+            startActivity(intent)
+        }
+
         val bundle = intent.extras
         if(bundle!=null){
             iv_pelicula_image.setImageResource(bundle.getInt("header"))
             tv_nombre_pelicula.setText(bundle.getString("titulo"))
             tv_pelicula_desc.setText(bundle.getString("sinopsis"))
+            seats.setText(boletos)
         }
 
     }
